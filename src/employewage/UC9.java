3fbf2cc87f2fda830e6f6c6a76562a250575fde4
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-26 13:23:09 +0530

    added uc9 problem

---
+++ src/employewage/UC9.java
@@ -0,0 +1,91 @@
+package employewage;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+
+class Company {
+	ArrayList<Integer> Wage = new ArrayList<Integer>(); // Arraylist to store Integer value
+	ArrayList<String> Wages = new ArrayList<String>(); // Arreylist to dtore String value
+
+	void check() {
+		// variable
+		int totalsalary = 0;
+		int maxRateInMonth = 100;
+		int maxHoursInMonth = 100;
+		int totalEmpHr = 0;
+		int totalWorkingDays = 0;
+		int empHrs = 0;
+		int finalsalary = 0;
+
+		System.out.println("Enter your company name ");
+		Scanner p2 = new Scanner(System.in);
+		String x = p2.nextLine();
+		System.out.println(x); // taking user input company name
+		Wages.add(x);
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Employee rate Per hour of your Company :");
+		int empRatePerHr1 = scanner.nextInt();
+
+		System.out.println("Enter the Number of working days of your Company:");
+		int numOfWorkingDays1 = scanner.nextInt();
+
+		System.out.println("Enter the Number of working hours of your Company:");
+		int numOfWorkingHrs1 = scanner.nextInt();
+
+		while (totalEmpHr <= maxRateInMonth && totalWorkingDays <= numOfWorkingDays1
+				&& numOfWorkingHrs1 <= maxHoursInMonth) {
+			totalWorkingDays++;
+			double randomCheck = Math.floor(Math.random() * 10) % 3;
+			int i = (int) randomCheck;
+			switch (i) {
+			case 2:
+				// System.out.println("Employee is FullTime");
+				empHrs = 8;
+				break;
+			case 1:
+				// System.out.println("Employee is PartTime");
+				empHrs = 4;
+				break;
+			case 0:
+				// System.out.println("Employee is Absent");
+				empHrs = 0;
+				break;
+			}
+
+			totalEmpHr = totalEmpHr + empHrs;
+			// System.out.println(totalEmpHr);
+
+		}
+		totalsalary = totalEmpHr * empRatePerHr1;
+		finalsalary += totalsalary;
+
+		Wage.add(finalsalary);
+		System.out.println(Wages);
+		System.out.println(Wage);
+
+	}
+
+}
+
+public class UC9 {
+
+	public static void main(String[] args) {
+
+		System.out.println("How many company data you want to Enter ");
+		// creating object for to call method
+		Company mahindra = new Company();
+
+		Scanner p1 = new Scanner(System.in);
+		p1.toString();
+		int noOfCompanys = p1.nextInt(); // taking for how many company you want to store data
+
+		for (int i = 1; i <= noOfCompanys; i++) {
+
+			mahindra.check(); // calling method in object
+
+		}
+	}
+
+}
